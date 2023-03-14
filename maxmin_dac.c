72ecbe56a3e19dd2d5d2d63bb9b670b060e7555a
Ritam Chakrabortty <107584853+ritamchakrabortty@users.noreply.github.com>
2023-03-14 10:41:00 +0530

    Add files via upload

---
+++ maxmin_dac.c
@@ -0,0 +1,56 @@
+#include<stdio.h> 
+#include<stdlib.h> 
+int max,min; 
+int *a; 
+void maxmin(int i, int j) 
+{ 
+ int max1, min1, mid; 
+ if(i==j) 
+ { 
+    max = min = a[i]; 
+ } 
+ else 
+ { 
+    if(i == j-1) 
+    { 
+        if(a[i] <a[j]) 
+        { 
+            max = a[j]; 
+            min = a[i]; 
+        } 
+        else 
+        { 
+            max = a[i]; 
+            min = a[j]; 
+        } 
+    } 
+    else 
+    { 
+        mid = (i+j)/2; 
+        maxmin(i, mid); 
+        max1 = max; min1 = min; 
+        maxmin(mid+1, j); 
+        if(max <max1) 
+            max = max1; 
+        if(min > min1) 
+            min = min1; 
+    } 
+ } 
+} 
+int main () 
+{ 
+ int i, num; 
+ printf ("\nEnter the size of the array : "); 
+ scanf ("%d",&num); 
+ a=(int *)malloc((num+1)*sizeof(int)); 
+ printf ("Enter the elements of the array : \n"); 
+ for (i=1;i<=num;i++) 
+    scanf ("%d",&a[i]); 
+ 
+ max = a[0]; 
+ min = a[0]; 
+ maxmin(1, num); 
+ printf ("Minimum element in an array : %d\n", min); 
+ printf ("Maximum element in an array : %d\n", max); 
+ return 0; 
+}
